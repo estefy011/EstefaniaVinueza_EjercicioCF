f72075efba376575be4991ebfddb23adbb4c71f4
estefy011 <jadira.vinueza@udla.edu.ec>
2023-01-28 15:17:45 -0500

    cambios

--- EstefaniaVinueza_EjercicioCF/Program.cs
+++ EstefaniaVinueza_EjercicioCF/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using EstefaniaVinueza_EjercicioCF.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<EstefaniaVinueza_EjercicioCFContext>(options =>
